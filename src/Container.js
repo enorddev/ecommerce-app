a2dff76a5ae88b50b5c3b4a02ee863eda02f26a5
Edwin Nord <edwin.nord09@gmail.com>
2022-10-20 21:46:37 -0400

    feature update

--- src/Container.js
+++ src/Container.js
@@ -1,18 +1,19 @@
-import React , {useState} from 'react'
+import React , {useState} from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import closeBtn from './images/icon-close.svg'
+import closeBtn from './images/icon-close.svg';
 import { Carousel } from 'react-responsive-carousel';
-import hamburger from './images/icon-menu.svg'
-import cart from './images/icon-cart.svg'
-import avatar from './images/image-avatar.png'
-import logo from './images/logo.svg'
-import deleteBtn from './images/icon-delete.svg'
-import buttonCart from './images/icon-cart.svg'
+import hamburger from './images/icon-menu.svg';
+import cart from './images/icon-cart.svg';
+import avatar from './images/image-avatar.png';
+import logo from './images/logo.svg';
+import deleteBtn from './images/icon-delete.svg';
+import buttonCart from './images/icon-cart.svg';
+import * as ReactBootStrap from "react-bootstrap";
 
 
 
 
-export default function Container() {
+export default function Hero() {
     const [output, setOutput] = useState(0)
     const [cartOutput, setCartOutput] = useState(0)
     const [showModal, setShowModal] = useState(false)
@@ -78,103 +79,124 @@ export default function Container() {
       
 
   return (
-    <div>
-   
+        <>      
+            
         <header>
-          <div className='flex-left'>
-            <div className='navigation'>
-                <div className='burger-menu'>
-                    <img  src={hamburger} />
-                    
-                </div>
+             
+             {[ 'xl'].map((expand) => (
+             <ReactBootStrap.Navbar key={expand} variant='light' expand={expand} className="mb-3" id='navbar'>
+                <ReactBootStrap.Container fluid>
+                    <ReactBootStrap.Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <ReactBootStrap.Navbar.Brand ><img src={logo}/></ReactBootStrap.Navbar.Brand>
+                            <ReactBootStrap.Navbar.Offcanvas
+                                id={`offcanvasNavbar-expand-${expand}`}
+                                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                                placement="end">
+                                <ReactBootStrap.Offcanvas.Header closeButton></ReactBootStrap.Offcanvas.Header>
+                        <ReactBootStrap.Offcanvas.Body>
+                            <ReactBootStrap.Nav className="justify-content-center flex-grow-1 pe-3">
+                                <ReactBootStrap.Nav.Link href="#action1">Collections</ReactBootStrap.Nav.Link>
+                                <ReactBootStrap.Nav.Link href="#action2">Men</ReactBootStrap.Nav.Link>
+                                <ReactBootStrap.Nav.Link href="#action3">Women</ReactBootStrap.Nav.Link>
+                                <ReactBootStrap.Nav.Link href="#action4">About</ReactBootStrap.Nav.Link>
+                                <ReactBootStrap.Nav.Link href="#action5">Contact</ReactBootStrap.Nav.Link>
+                            </ReactBootStrap.Nav>
+                        </ReactBootStrap.Offcanvas.Body>
+                    </ReactBootStrap.Navbar.Offcanvas>
+                    <ReactBootStrap.Navbar.Brand href= '#'>
+                    <ReactBootStrap.Navbar.Text className="justify-content-end" id="navbar-align"/>
+                        <span className='item-output'>
+                            {cartOutput}
+                        </span>
+                            <img 
+                                className='cart-image'
+                                width='20' 
+                                height='20'
+                                style={{
+                                cursor:'pointer', 
+                                position: 'relative'
+                                }} 
+                                onClick={handleCart} 
+                                src={cart}/>
+
+                            <img 
+                                className='avatar' 
+                                
+                                width='40' 
+                                height='40'
+                                src={avatar}/>
+                    </ReactBootStrap.Navbar.Brand>
                 
-            </div>
-            <img src={logo}/>
-          </div>
-                    
+                </ReactBootStrap.Container>
+            </ReactBootStrap.Navbar>
+            ))}
+    </header>
             
-          
 
-          <div className='flex-right'>
-            <div className='cart-flex'>
-            <span className='item-output'>
-                {cartOutput}
-              </span>
-            <img style={{
-                        cursor:'pointer', 
-                        position: 'relative'
-                        }} 
-                        onClick={handleCart} 
-                        src={cart}/>
-              
-              
-            </div>
-            <img className='avatar' src={avatar}/>
-          </div>
-        </header>
+        <div className='wrapper'>
+            <div>
+            {showModal && 
+                <div className='cart-modal'>
+                    <div className={`modal-content ${isVisible ? 'isVisible' : <h1>Empty</h1>}`}>
 
-        
+                        <h3 style={{textAlign:'center', paddingBottom: '10px', paddingTop: '10px'}}>Cart</h3>
+                        <hr />
+                        <div style={{
+                                    display:'flex', 
+                                    alignItems: 'center', 
+                                    justifyContent:'center', 
+                                    gap:'25px', 
+                                    width:'100%', 
+                                    maxWidth:'350px', 
+                                    margin:'auto',
+                                    paddingTop: '25px'
+                                    }}>
+                            <img style={{borderRadius:'8px'}} src='./main3.webp' width='80'/>
+                            <div style={{display:'flex', flexDirection: 'column'}}>
+                                <p style={{fontWeight:'400'}}>Fall Limited Edition Sneakers</p>
+                                <p style={{display:'flex', gap:'50px'}}>${price}.00 x {output}  <strong>${totalPrice}.00</strong></p>
+                            </div>
 
-        {showModal && 
-            <div className='cart-modal'>
-                <div className={`modal-content ${isVisible ? 'isVisible' : <h1>Empty</h1>}`}>
-
-                    <h3 style={{textAlign:'center', paddingBottom: '10px', paddingTop: '10px'}}>Cart</h3>
-                    <hr />
-                    <div style={{
-                                display:'flex', 
-                                alignItems: 'center', 
-                                justifyContent:'center', 
-                                gap:'25px', 
-                                width:'100%', 
-                                maxWidth:'350px', 
-                                margin:'auto',
-                                paddingTop: '25px'
-                                }}>
-                        <img style={{borderRadius:'8px'}} src='./main3.webp' width='80'/>
-                        <div style={{display:'flex', flexDirection: 'column'}}>
-                            <p style={{fontWeight:'400'}}>Fall Limited Edition Sneakers</p>
-                            <p style={{display:'flex', gap:'50px'}}>${price}.00 x {output}  <strong>${totalPrice}.00</strong></p>
+                            <img onClick={handleDelete} src={deleteBtn}/>
                         </div>
-
-                        <img onClick={handleDelete} src={deleteBtn}/>
+                            <div className='checkout-Btn'>
+                                <button >Checkout</button>
+                            </div>
                     </div>
-                        <div className='checkout-Btn'>
-                            <button >Checkout</button>
-                        </div>
+
+                </div>}
+
+                {isEmpty && <div className={`empty-Cart ${isEmpty ? 'isEmpty' : ''}`}><h1>Your cart is empty</h1></div>}
+            
+
+                
+            {/* HEADER SECTION ENDS HERE */}
+            
+            
+            {/* IMAGE CAROUSEL SECTION */}
+            
+            <Carousel className={`carousel ${showModal ? "showModal" : ""}`}>
+                <div> 
+                    <img src="./main1.webp" />
                 </div>
 
-            </div>}
+                <div>
+                    <img src="./main2.webp" />
+                </div>
 
-            {isEmpty && <div className={`empty-Cart ${isEmpty ? 'isEmpty' : ''}`}><h1>Your cart is empty</h1></div>}
+                <div>
+                    <img src="./main3.webp" />
+                </div>
+
+                <div>
+                    <img src="./main4.webp" />
+                </div>
+            </Carousel>
+        </div>
         
-
-            
-        {/* HEADER SECTION ENDS HERE */}
-        
-        
-        {/* IMAGE CAROUSEL SECTION */}
-        
-        <Carousel className={`carousel ${showModal ? "showModal" : ""}`}>
-            <div> 
-                <img src="./main1.webp" />
-            </div>
-
-            <div>
-                <img src="./main2.webp" />
-            </div>
-
-            <div>
-                <img src="./main3.webp" />
-            </div>
-
-            <div>
-                <img src="./main4.webp" />
-            </div>
-        </Carousel>
 
         {/* PRODUCT DESCRIPTION SECTION */}
-        
+        <div>
         <div className='desc'>
             <h3 style={spanStyle}>SNEAKER COMPANY</h3>
             <h1 style={{width:'100%', maxWidth:'400px', paddingTop:'15px'}}>Fall Limited Edition Sneakers</h1>
@@ -219,7 +241,44 @@ export default function Container() {
 
         
         </div>
+        </div>
+        
       
     </div>
+        </>
+           
   )
 }
+
+
+
+{/* <div className='flex-left'>
+                <div className='navigation'>
+                    <div className='burger-menu'>
+                        <img  src={hamburger} />
+                        
+                    </div>
+                    
+                </div>
+                <img src={logo}/>
+            </div>
+                        
+                
+            
+
+            <div className='flex-right'>
+                <div className='cart-flex'>
+                <span className='item-output'>
+                    {cartOutput}
+                </span>
+                <img style={{
+                            cursor:'pointer', 
+                            position: 'relative'
+                            }} 
+                            onClick={handleCart} 
+                            src={cart}/>
+                
+                
+                </div>
+                />
+            </div> */}
